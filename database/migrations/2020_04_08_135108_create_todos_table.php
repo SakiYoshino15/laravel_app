6c9898fe8e3f237509fbaac257f57c35da942fd3
吉野早紀 <xihuan15caomei@gmail.com>
2020-04-09 12:08:31 +0900

    テーブル再作成のため、migrationsファイル新規作成とseederファイル追記

---
+++ database/migrations/2020_04_08_135108_create_todos_table.php
@@ -0,0 +1,4 @@
+Unstaged changes after reset:
+D	database/migrations/2020_02_22_094655_create_todos_table.php
+D	database/migrations/2020_03_18_074924_add_user_id_to_todos_table.php
+M	database/migrations/2020_04_08_135108_create_todos_table.php
--- database/seeds/TodosTableSeeder.php
+++ database/seeds/TodosTableSeeder.php
@@ -15,22 +15,26 @@ class TodosTableSeeder extends Seeder
         DB::table('todos')->truncate();
         DB::table('todos')->insert([
             [
+                'user_id'         => 6,
                 'title'      => 'Gitテスト',
                 'created_at' => Carbon::create(2018, 1, 1),
                 'updated_at' => Carbon::create(2018, 1, 4),
             ],
             [
+                'user_id'         => 7,
                 'title'      => 'Gitの作業フローに慣れる',
                 'created_at' => Carbon::create(2018, 2, 1),
                 'updated_at' => Carbon::create(2018, 2, 5),
             ],
             [
-                'title'      => 'Gitの作業フローに慣れる',
+                'user_id'         => 8,
+                'title'      => 'git diffコマンドを使いこなす',
                 'created_at' => Carbon::create(2018, 2, 1),
                 'updated_at' => Carbon::create(2018, 2, 5),
             ],
             [
-                'title'      => 'Gitの作業フローに慣れる',
+                'user_id'         => 9,
+                'title'      => 'Gitの課題に取り組む',
                 'created_at' => Carbon::create(2018, 2, 1),
                 'updated_at' => Carbon::create(2018, 2, 5),
             ],
