61cec8c3487f9f414514b09c6f24cb2785a523a0
吉野早紀 <xihuan15caomei@gmail.com>
2020-04-10 15:40:32 +0900

    追加課題Lv１

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,8 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::resource('todo', 'TodoController');
+
+Route::resource('/gizumo_lv1', 'LessonController');
 
 Auth::routes();
 
